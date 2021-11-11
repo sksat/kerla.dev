80c9d83a9817b38c4dae7935554b1dfc44014832
Seiya Nuta <nuta@seiya.me>
2021-11-11 22:56:56 +0900

    Add build-src-docs.sh

---
+++ build-src-docs.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+set -ue
+
+if [ ! -d kerla ]; then
+    git clone https://github.com/nuta/kerla
+fi
+
+cd kerla
+git pull
+
+curl https://sh.rustup.rs -sSf | sh -s -- -y
+source $HOME/.cargo/env
+
+rustup override set nightly
+rustup component add rust-src
+
+touch build/kerla.initramfs
+make src-docs
+mv target/doc ../public
